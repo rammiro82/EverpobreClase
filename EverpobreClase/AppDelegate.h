aeb3a6fcc1541e865e7d1b082d142c52da308f3b
Ramiro García Salazar <rammiro82@gmail.com>
2016-01-16 22:58:36 +0100

    Initial Commit

---
+++ EverpobreClase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EverpobreClase
+//
+//  Created by Ramiro García Salazar on 16/1/16.
+//  Copyright © 2016 Ramiro García Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
