ccdc52caf07c8f437a49816b1ca5010c56476710
Ramiro García Salazar <rammiro82@gmail.com>
2016-01-30 23:36:52 +0100

    Creando la celda personalizada

---
+++ EverpobreClase/RGSNoteCellView.h
@@ -0,0 +1,13 @@
+//
+//  RGSNoteCellView.h
+//  EverpobreClase
+//
+//  Created by Ramiro García Salazar on 30/1/16.
+//  Copyright © 2016 Ramiro García Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGSNoteCellView : UICollectionViewCell
+
+@end
---
+++ EverpobreClase/RGSNoteCellView.m
@@ -0,0 +1,13 @@
+//
+//  RGSNoteCellView.m
+//  EverpobreClase
+//
+//  Created by Ramiro García Salazar on 30/1/16.
+//  Copyright © 2016 Ramiro García Salazar. All rights reserved.
+//
+
+#import "RGSNoteCellView.h"
+
+@implementation RGSNoteCellView
+
+@end
